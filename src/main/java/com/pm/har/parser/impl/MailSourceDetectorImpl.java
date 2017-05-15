8ba4c279df7bd8bc2fc57721703ca9f1ffbe0a53
Andrew Kolpakov <andrew.kolpakov@gmail.com>
2017-05-15 10:19:41 +0700

    make mail source detection more reliable

--- src/main/java/com/pm/har/parser/impl/MailSourceDetectorImpl.java
+++ src/main/java/com/pm/har/parser/impl/MailSourceDetectorImpl.java
@@ -9,16 +9,16 @@ import java.util.regex.Pattern;
 @Service
 public class MailSourceDetectorImpl implements MailSourceDetector {
     private Pattern homes = Pattern.compile("homes.com");
-    private Pattern har = Pattern.compile("har.com");
+    private Pattern har = Pattern.compile("(?i)har.com lead");
     private Pattern move = Pattern.compile("move.com");
 
     @Nullable
     @Override
     public Source detectSource(String content) {
-        if (homes.matcher(content).find()) {
-            return Source.HOMES;
-        } else if (har.matcher(content).find()) {
+        if (har.matcher(content).find()) {
             return Source.HAR;
+        } else if (homes.matcher(content).find()) {
+            return Source.HOMES;
         } else if (move.matcher(content).find()) {
             return Source.MOVES;
         }
